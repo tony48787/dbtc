e7770ad2b3599a78f8387e114b509070c0f84796
Prakhar Rathi <>
2019-11-01 14:26:23 +0800

    Added add/edit screen

--- lib/screens/add_edit_screen.dart
+++ lib/screens/add_edit_screen.dart
@@ -1,21 +1,87 @@
+import 'package:dbtc/models/Task.dart';
 import 'package:flutter/material.dart';
 
-class AddEditScreen extends StatelessWidget {
+typedef OnSaveCallback = Function(String task, String note);
+
+class AddEditScreen extends StatefulWidget {
+  final Task task;
+  bool isEditing;
+
+  AddEditScreen({ this.task }) {
+    isEditing = task != null;
+  }
+
+  @override
+  _AddEditScreenState createState() => _AddEditScreenState();
+
+  void onSave(BuildContext context, String title, String description) {
+    print(title);
+    print(description);
+    Navigator.pop(context);
+  }
+}
+
+class _AddEditScreenState extends State<AddEditScreen> {
+  static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _title;
+  String _description;
+
+  bool get isEditing => widget.isEditing;
+
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
+        title: Text(
+          isEditing ? "Edit" : "Add",
         ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                initialValue: isEditing ? widget.task.title : '',
+                autofocus: !isEditing,
+                style: textTheme.headline,
+                decoration: InputDecoration(
+                  hintText: "Name",
+                ),
+                validator: (val) {
+                  return val.trim().isEmpty
+                      ? "This is required"
+                      : null;
+                },
+                onSaved: (value) => _title = value,
+              ),
+              TextFormField(
+                initialValue: isEditing ? widget.task.description : '',
+                maxLines: 10,
+                style: textTheme.subhead,
+                decoration: InputDecoration(
+                  hintText: "Description",
+                ),
+                onSaved: (value) => _description = value,
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: isEditing ? "Save changes" : "Add task",
+        child: Icon(isEditing ? Icons.check : Icons.add),
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            _formKey.currentState.save();
+            widget.onSave(context, _title, _description);
+          }
+        },
       ),
     );
   }
-
 }
