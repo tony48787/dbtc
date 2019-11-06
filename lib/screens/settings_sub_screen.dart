f87d6a2d127ee9609fb7436bf53407f35d7a249e
tony48787 <tony65548711tony@yahoo.com.hk>
2019-11-06 18:15:37 +0800

    Add time picker for selecting notification

--- lib/screens/settings_sub_screen.dart
+++ lib/screens/settings_sub_screen.dart
@@ -1,22 +1,89 @@
 import 'package:dbtc/blocs/theme/theme.dart';
+import 'package:dbtc/localizations/app_localizations.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsSubScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
         ListTile(
           leading: Icon(Icons.brightness_medium),
-          title: Text('Dark theme'),
+          title: Text(AppLocalizations.of(context).translate('Dark Theme')),
           onTap: () {
             BlocProvider.of<ThemeBloc>(context).add(ThemeChanged());
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.brightness_medium),
+          title: Text(AppLocalizations.of(context).translate('NOTIFICATION_TIME')),
+          onTap: () {
+            showNotificationDialog(context);
           },
         ),
       ],
     );
   }
 
+  Future<void> showNotificationDialog(BuildContext parentContext) async {
+    DateTime _dateTime = DateTime.now();
+
+    return showDialog<void>(
+      context: parentContext,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(child: Text(AppLocalizations.of(context).translate('NOTIFICATION_TIME'))),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                timePickerWidget(context, (DateTime newDateTime) {
+                  _dateTime = newDateTime;
+                })
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(AppLocalizations.of(context).translate('SAVE')),
+              onPressed: () {
+                Navigator.of(context).pop();
+
+//                _showNotificationWithoutSound();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget timePickerWidget(BuildContext context, _onDateTimeChanged) {
+    final theme = CupertinoTheme.of(context);
+
+    return CupertinoTheme(
+      data: theme.copyWith(
+        textTheme: CupertinoTextThemeData(
+          dateTimePickerTextStyle: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      child: Container(
+        height: MediaQuery
+            .of(context)
+            .size
+            .height / 6,
+        child: CupertinoDatePicker(
+          mode: CupertinoDatePickerMode.time,
+          onDateTimeChanged: _onDateTimeChanged,
+          use24hFormat: true,
+          minuteInterval: 1,
+        ),
+      )
+      ,
+    );
+  }
 }
