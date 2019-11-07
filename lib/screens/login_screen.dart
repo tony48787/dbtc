7a03f6f57340531440fd008de531fd185916e05d
Prakhar Rathi <>
2019-11-07 17:28:14 +0800

    Removed unnecessary code

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,20 +14,13 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Login')),
       body: BlocProvider<LoginBloc>(
         builder: (context) => LoginBloc(userRepository: _userRepository),
-        child: LoginForm(userRepository: _userRepository),
+        child: LoginForm(),
       ),
     );
   }
 }
 
 class LoginForm extends StatefulWidget {
-  final UserRepository _userRepository;
-
-  LoginForm({Key key, @required UserRepository userRepository})
-      : assert(userRepository != null),
-        _userRepository = userRepository,
-        super(key: key);
-
   State<LoginForm> createState() => _LoginFormState();
 }
 
@@ -36,8 +29,6 @@ class _LoginFormState extends State<LoginForm> {
   final TextEditingController _passwordController = TextEditingController();
 
   LoginBloc _loginBloc;
-
-  UserRepository get _userRepository => widget._userRepository;
 
   bool get isPopulated =>
       _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty;
@@ -135,7 +126,7 @@ class _LoginFormState extends State<LoginForm> {
                               : null,
                         ),
 //                        GoogleLoginButton(),
-                        CreateAccountButton(userRepository: _userRepository),
+                        CreateAccountButton(),
                       ],
                     ),
                   ),
@@ -197,13 +188,6 @@ class LoginButton extends StatelessWidget {
 }
 
 class CreateAccountButton extends StatelessWidget {
-  final UserRepository _userRepository;
-
-  CreateAccountButton({Key key, @required UserRepository userRepository})
-      : assert(userRepository != null),
-        _userRepository = userRepository,
-        super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return FlatButton(
